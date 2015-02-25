645372587c3a894130570d534baaea9a11ca699a
Chunfeng <CrazyPandar@gmail.com>
2015-02-26 01:34:05 +0800

    ds_log
    first working ds_log

--- src/include/ds_log.h
+++ src/include/ds_log.h
@@ -1,6 +1,6 @@
 /*
  * libds
- * Copyright (c) Chunfeng Zhang(CrazyPandar@gmail.com), All rights reserved.
+ * Copyright (c) Chunfeng Zhang <CrazyPandar@gmail.com>, All rights reserved.
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -18,7 +18,7 @@
  */
 #ifndef _DS_LOG_H_
 #define _DS_LOG_H_
-#include "tn_plat.h"
+#include "ds_plat_basic.h"
 /*
 	Define these macros before include this head file
 #define ERROR_LOG_FILE "/var/log/tn_error.log"
@@ -26,15 +26,13 @@
 #define DEBUG_LOG_FILE "/var/log/tn_debug.log"
 */
 
-enum DS_LOG_TYPE{LOG_DEBUG, LOG_MESSAGE, LOG_ERROR};
+enum DS_LOG_TYPE{LOG_DEBUG=0, LOG_MESSAGE, LOG_ERROR};
 int DSLogToFile(enum DS_LOG_TYPE type, const char* file_path, const char* format, ...);
-int DSLogCloseFile();
-
 
 #ifndef ERROR_LOG_FILE
 #define DS_ERR(...) fprintf(stderr, "[E][%s:%d] ", __FILE__, __LINE__); fprintf(stderr, __VA_ARGS__);
 #else
-#define DS_ERR(...) cf_log_file(LOG_ERROR, ERROR_LOG_FILE, "[E][%s:%d] ", __FILE__, __LINE__); cf_log_file(LOG_ERROR, ERROR_LOG_FILE, __VA_ARGS__);
+#define DS_ERR(...) DSLogToFile(LOG_ERROR, ERROR_LOG_FILE, "[E][%s:%d] ", __FILE__, __LINE__); DSLogToFile(LOG_ERROR, ERROR_LOG_FILE, __VA_ARGS__);
 #endif
 
 #define DS_SYS_ERR(...)   {DS_ERR(__VA_ARGS__);DS_ERR("For %s\n", strerror(errno));}
@@ -42,18 +40,17 @@ int DSLogCloseFile();
 #define DS_SYS_ERR_OUT(_dest, ...)   {DS_SYS_ERR(__VA_ARGS__); goto _dest;}
 
 
-
 #ifndef MESSAGE_LOG_FILE
 #define DS_MSG(...) fprintf(stdout, "[M][%s:%d] ", __FILE__, __LINE__);	fprintf(stdout, __VA_ARGS__);
 #else
-#define DS_MSG(...) cf_log_file(LOG_MESSAGE, MESSAGE_LOG_FILE, "[M][%s:%d] ", __FILE__, __LINE__); cf_log_file(LOG_MESSAGE, MESSAGE_LOG_FILE, __VA_ARGS__);
+#define DS_MSG(...) DSLogToFile(LOG_MESSAGE, MESSAGE_LOG_FILE, "[M][%s:%d] ", __FILE__, __LINE__); DSLogToFile(LOG_MESSAGE, MESSAGE_LOG_FILE, __VA_ARGS__);
 #endif
 
 
 #ifndef DEBUG_LOG_FILE
 #define DS_DBG(...) fprintf(stdout, "[D][%s:%d] ", __FILE__, __LINE__); fprintf(stdout, __VA_ARGS__);
 #else
-#define DS_DBG(...) cf_log_file(LOG_DEBUG, DEBUG_LOG_FILE, "[D][%s:%d]", __FILE__, __LINE__); cf_log_file(LOG_DEBUG, DEBUG_LOG_FILE, __VA_ARGS__);
+#define DS_DBG(...) DSLogToFile(LOG_DEBUG, DEBUG_LOG_FILE, "[D][%s:%d]", __FILE__, __LINE__); DSLogToFile(LOG_DEBUG, DEBUG_LOG_FILE, __VA_ARGS__);
 #endif
 
 #endif
